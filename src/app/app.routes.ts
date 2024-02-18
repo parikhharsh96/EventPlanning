24a19ab963abc86c97bddfd13efbfcc885f39a99
harshcrosspay <harsh@crosspaygroup.com>
2024-02-19 02:28:51 +0530

    initial commit for event planning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'home', pathMatch: 'full'},
+    {path: 'home', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent)},
+    {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(mod => mod.DashboardComponent)},
+    
+];
